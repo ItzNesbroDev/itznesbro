9297ddabe09985fbe4acb84de28dba70a5a7a5fd
ItzNesbro Dev <asnesbeer3@gmail.com>
2023-07-28 02:47:33 +0000

    updated works page

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -10,20 +10,20 @@
 </svelte:head>
 
 <div class="container mx-auto py-10">
-	<h1 class="text-4xl font-bold mb-10">My Works</h1>
+	<h1 class="text-4xl font-bold mb-10 ml-5 underline text-white">My Works</h1>
 	<div class="grid grid-cols-1 md:p-6 md:grid-cols-2 lg:grid-cols-3 gap-8">
-		{#each projects as project (project.url)}
-			<div class="bg-white rounded-lg shadow-lg overflow-hidden" key={project.url}>
-				<div class="p-6">
-					<h2 class="text-2xl font-bold mb-4">{project.name}</h2>
-					<p class="text-gray-600">{project.description}</p>
-					<a
-						href={project.url}
-						class="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 mt-4"
-					>
-						View Project
-					</a>
-				</div>
+		{#each projects as project}
+			<div class="bg-white rounded-lg shadow-lg p-6">
+				<h2 class="text-xl font-bold mb-2">{project.name}</h2>
+				<p class="text-gray-500 mb-4">{project.description}</p>
+				<a
+					href="{project.url}"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-blue-500 hover:text-blue-700 mt-2"
+				>
+					GitHub Repo
+				</a>
 			</div>
 		{/each}
 	</div>
